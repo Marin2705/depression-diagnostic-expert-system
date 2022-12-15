2b739545e6c8c9312a0d266b6376283de4d87395
Marin2705 <40020721+Marin2705@users.noreply.github.com>
2022-12-15 14:41:10 +0100

    add printQuestion

--- BF.lisp
+++ BF.lisp
@@ -1,9 +1,12 @@
 ;;;     BASE DE FAITS
 
-(defparameter *BF* (list
-    '(= genre 'homme)
-    '(= age 97)
-    '(= troublePhysiqueOuCognitif T)
-    ; '(= nombreTroublesSecondaires 2)
-    ; '(= nombreTroublesPrincipaux 1)
-    ))
+; (defparameter *BF* (list
+;     '(= genre 'homme)
+;     '(= age 97)
+;     '(= troublePhysiqueOuCognitif T)
+;     ; '(= nombreTroublesSecondaires 2)
+;     ; '(= nombreTroublesPrincipaux 1)
+;     ))
+
+
+(defparameter *BF* ())
--- main.lisp
+++ main.lisp
@@ -1,7 +1,7 @@
 ;;; import
 (load "engine.lisp")
 
-;;;     BUT
+;;; BUT
 
 (defparameter *but* '(= etatDepressif T))
 
@@ -18,13 +18,49 @@
     (terpri)
 )
 
+;;; questions
+
+(defparameter *questions* (list
+    '(nombreTroublesPrincipaux "nombre de troubles principaux" numberp)
+    '(nombreTroublesSecondaires "nombre de troubles secondaires" numberp)
+    '(moisDepuisAccouchement "nombre de mois depuis l'accouchement" numberp)
+    '(symptomesPostnatale "nombre de symptomes de la dépression post-natale" numberp)
+    ; (list 'genre "votre genre ? (H/F)" (list 'lambda '(x) (list 'or (list 'equalp 'x "H") (list 'equalp 'x "F")))) ;  (list 'lambda '(x) (list '+ 'x n)))
+    '(age "votre age ?" numberp)
+    ; '(troublePhysiqueOuCognitif "avez-vous un trouble physique ou cognitif ? (true/false)" (lambda (x) (or (equalp x "true") (equalp x "false"))))
+))
+
+
+(defun printQuestion(question)
+    (terpri)
+    (write-string (cadr question))
+    (terpri)
+    (write-string "Type your answer.")
+    (terpri)
+    (let ((userInput (read)))
+        (if (funcall (caddr question) userInput) ;; validate user 
+            (progn
+                (pushnew (list '= (car question) userInput) *BF*))
+            (progn
+                (write-string "Wrong input. Please try again.")
+                (printQuestion question)
+            )
+        )
+    )
+)
+
 (defun menu()
 	(printMenu)
     (terpri)
     (let ((action (read)))
         (if (numberp action) 
             (cond   ((= action 1)
-                        (write-string (verifier *but*)))
+                        (progn 
+                            (setf *BF* ())
+                            (dolist (question *questions*)
+                                (printQuestion question)
+                            )
+                            (write-string (verifier *but*))))
                         ((= action 2)
                         (quit))
                         ((write-string "Wrong input. Please try again with a number included in the menu."))
