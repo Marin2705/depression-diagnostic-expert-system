1a8cbb0c7ff13da96d1478f6014d464142752c97
CoulonLo <120654340+CoulonLo@users.noreply.github.com>
2023-01-11 20:43:25 +0100

    Update BR.lisp

--- BR.lisp
+++ BR.lisp
@@ -1,47 +1,39 @@
 ; Base de règles
 
 (defparameter *BR* (list
-    '(R1 ((sensation_depression eq intellectuelle)) 'triste))
-    '(R2 ((type_activite eq physique)) 'manque_interer))
-    '(R3 ((type_intellectuelle eq creative)) 'trouble_sommeil))
-    '(R4 ((type_intellectuelle eq culturelle)) 'trouble_appetit))
-    '(R5 ((type_intellectuelle eq reflexive)) 'difficulte_concentration))
-    '(R6 ((type_intellectuelle eq detente)) 'faible_estime))
-    '(R7 ((type_intellectuelle eq detente)) 'causes_externes_fortes))
-
- '("avez-vous la sensation d'être dépressif ? (OUI ou NON)" 'sensation_depression)
-    '("Ressentez-vous un sentiment ou des sensations semblables a la tristesse ?(oui ou non)" 'tristesse)
-    '("Avez-vous perte d'intérêt en géneral ?(oui ou non)" 'faible_interet)
-    '("Resentez-vous un trouble en particulier ? (sommeil,appetit ou concentration)" 'trouble)
-    '("Avez-vous faible estime de vous ?(oui ou non)" 'estime_de_soi)
-    '("Avez-vous vécu un évenement traumatisant ? Si oui notez l'importance de celle-ci sur 10 sinon mmettez 0." 'evennement_traumatisant)
+    '(R1 ((sensation_depression eq oui)) 'triste))
+    '(R2 ((faible_interet eq oui)) 'manque_interer))
+    '(R3 ((trouble eq sommeil)) 'trouble_sommeil))
+    '(R4 ((trouble eq appetit)) 'trouble_appetit))
+    '(R5 ((trouble eq concentration)) 'trouble_concentration))
+    '(R6 ((estime_de_soi eq oui)) 'faible_estime))
+    '(R7 ((evennement_traumatisant >= 5)) 'causes_externes_fortes))
 
 ;;La dépression réactionnelle
 
-    '(R8 ((type_intellectuelle eq detente)) 'perte_emotionnelle))
-    '(R9 ((type_intellectuelle eq detente)) 'ralentissement_psychomoteur))
-    '(R10 ((type_intellectuelle eq detente)) 'perte_libido))
-    '(R11 ((type_intellectuelle eq detente)) 'pas_causes_externes))
-    '(R12 ((type_intellectuelle eq detente)) 'intensité_symptomes_forte))
+    '(R8 ((empathie eq oui)) 'perte_emotionnelle))
+    '(R9 ((ralentissement_psychomoteur eq oui)) 'ralentissement_psychomoteur))
+    '(R10 ((libido eq oui)) 'perte_libido))
+    '(R11 ((evennement_traumatisant = 0) 'pas_causes_externes))
+    '(R12 ((intensite >= 5)) 'intensité_symptomes_forte))
 
 ;;La dépression endogène
 
-    '(R13 ((type_intellectuelle eq detente)) 'intensité_symptomes_faible))
-    '(R14 ((type_intellectuelle eq detente)) 'sensible_environnement)) 
-    '(R15 ((type_intellectuelle eq detente)) 'causes_externes_faibles))
+    '(R13 ((intensite < 5)) 'intensité_symptomes_faible))
+    '(R14 ((environnement eq oui)) 'sensible_environnement)) 
+    '(R15 ((evennement_traumatisant < 5) (evennement_traumatisant != 0)) 'causes_externes_faibles))
 
 ;;La dépression névrotique
 
-    '(R16 ((type_intellectuelle eq detente)) 'acouchement_4_8_semaines))
-
+    '(R16 ((sexe eq femme) (nb_acouchement > 4) (nb_acouchement < 8)) 'acouchement_4_8_semaines))
 
 ;;La dépression du post-partum
     
-    '(R17 ((type_intellectuelle eq detente)) 'vieu))
-    '(R18 ((type_intellectuelle eq detente)) 'vieille))
-    '(R19 ((type_intellectuelle eq detente)) 'mort_conjoint))
-    '(R20 ((type_intellectuelle eq detente)) 'maladie))
-    '(R21 ((type_intellectuelle eq detente)) 'faible_famille))
+    '(R17 ((sexe eq homme) (age > 79)) 'vieu_sensible))
+    '(R18 ((sexe eq femme) (age > 85)) 'vieille_sensible))
+    '(R19 ((décé eq oui)) 'mort_conjoint))
+    '(R20 ((maladies eq oui)) 'maladie))
+    '(R21 ((famille eq oui)) 'faible_famille))
 
 ;;La dépression de la personne âgée
 
