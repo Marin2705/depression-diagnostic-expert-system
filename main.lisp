ced4c6a0265b77df0f66f5cc3fed4bc8d16ec64c
CoulonLo <120654340+CoulonLo@users.noreply.github.com>
2023-01-11 15:14:18 +0100

    Update main.lisp

--- main.lisp
+++ main.lisp
@@ -1,12 +1,9 @@
 ;;; import
 (load "engine.lisp")
-(load "questions.lisp")
 
-; set *goal*
-
-(defparameter *goal* '(= etatDepressif T))
-
-; run engine (with interface) :
+;; Lancement :
+(progn
+  (format t "~%Bienvenue au cabinet...")
 
 (defun printMenu()
     (terpri)
@@ -14,60 +11,31 @@
     (terpri)
 	(write-line "1. Lancer le système expert")
 	(write-line "2. Sortir du système expert"))
-
-(defun printQuestion(question)
+	
+(defun printMenuExpertType()
     (terpri)
-    (dolist (text (cadr question))
-        (write-line text))
+	(write-line "------ Menu ------")
     (terpri)
-    (write-string "Réponses possibles : ")
-    (if (listp (caddr question))
-        (dolist (possibleAnswer (caddr question))
-            (format t "~%- ~a" possibleAnswer))
-        (write-string (caddr question)))
-    (terpri)
-    (write-line "Entrez votre réponse :")
-    (terpri)
-    (let ((userInput (read)) (answerValue))
-        ;; validate user input and push to BF
-        (if (listp (caddr question))
-            (dolist (possibleAnswer (caddr question))
-                (if (eq userInput possibleAnswer)
-                    (progn
-                        (pushnew (list '= (car question) NIL) *BF*)
-                        (return-from printQuestion))))
-            (if (funcall (nth 3 question) userInput)
-                (progn
-                    (pushnew (list '= (car question) userInput) *BF*)
-                    (return-from printQuestion))))
-        (write-line "Entrée invalide. Merci d'entrer une valeur proposée dans le menu")
-        (printQuestion question)))
+	(write-line "1. Lancer une recherche de chaînage arrière")
+	(write-line "2. Lancer une recherche de chaînage avant"))
 
 (defun menu()
 	(printMenu)
     (terpri)
-    (let ((action (read)))
-        (if (numberp action) 
-            (cond   ((= action 1)
-                        (progn 
-                            (setf *BF* ())
-                            (let ((indexQuestion 0) (questionsLength (list-length *questions*)))
-                                (loop for question in *questions*
-                                    do (progn (incf indexQuestion)
-                                        (printQuestion question)
-                                        (when (runEngine *goal*)
-                                            (progn
-                                                (terpri)
-                                                (write-line "Vous présentez les symptomes d'une dépression.")
-                                                (return)))))
-                                (if (eq indexQuestion questionsLength) 
-                                    (progn 
-                                        (terpri)
-                                        (write-line "Vous ne présentez pas les symptomes d'une dépression mais vous pouvez consulter un médecin pour confirmer l'analyse de vos symptômes."))))))
-                    ((= action 2)
-                        (quit))
-                    ((write-line "Entrée invalide. Merci d'entrer une valeur proposée dans le menu.")))
-            (write-line "Entrée invalide. Merci d'entrer une valeur proposée dans le menu.")))
+    (let ((action_1 (read)))
+        (if (numberp action_1) 
+            (cond ((= action_1 1)
+				(printMenuExpertType)
+				(terpri)
+				(let ((action_2 (read)))
+        		(if (numberp action_2) 
+            		(cond ((= action_2 1)
+                        (runEngine 1))
+						((= action_2 2)
+						(runEngine 2))))))
+				(= action_1 2)
+					(quit)))))
+    ((write-line "Entrée invalide. Merci d'entrer une valeur proposée dans le menu."))
+    (write-line "Entrée invalide. Merci d'entrer une valeur proposée dans le menu.")
 	(menu))
-
 (menu)
