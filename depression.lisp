e9cb0bfbdfad0992c97c0738f959bdf44d517a11
CoulonLo <120654340+CoulonLo@users.noreply.github.com>
2023-01-11 20:52:20 +0100

    Update depression.lisp

--- depression.lisp
+++ depression.lisp
@@ -4,3 +4,11 @@
   '(((intellectuelle)(creative)(musical)) "Composition musicale" "Faites parler votre créativité dans le domaine musicale !"))
   '(((intellectuelle)(creative)(litteraire)) "Ecriture" "Prennez un papier et un crayon et exprimez-vous librement !"))
 ))
+
+'(R1 ((sensation_depression eq oui)) 'triste))
+    '(R2 ((faible_interet eq oui)) 'manque_interer))
+    '(R3 ((trouble eq sommeil)) 'trouble_sommeil))
+    '(R4 ((trouble eq appetit)) 'trouble_appetit))
+    '(R5 ((trouble eq concentration)) 'trouble_concentration))
+    '(R6 ((estime_de_soi eq oui)) 'faible_estime))
+    '(R7 ((evennement_traumatisant >= 5)) 'causes_externes_fortes))
