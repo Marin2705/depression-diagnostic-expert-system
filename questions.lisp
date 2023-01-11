fc7277054a6e66384a995cff26cb1d2e7e512728
CoulonLo <120654340+CoulonLo@users.noreply.github.com>
2023-01-11 19:38:36 +0100

    Update questions.lisp

--- questions.lisp
+++ questions.lisp
@@ -1,57 +1,20 @@
-; symptomes principaux :
-
-    ; une tristesse constante
-        ; une humeur dépressive qui dure presque toute la journée et qui se répète pratiquement tous les jours depuis au moins deux semaines. Elle peut s'accompagner de pleurs ;
-    ; un abattement et une perte d'intérêt et de plaisir
-        ; pour des activités du quotidien et aussi pour les activités habituellement agréables ;
-    ; une réduction de l'énergie ou une fatigabilité anormale.
-        ; La personne souffre d'une fatigue intense, souvent dès le matin et a l'impression de manquer d'énergie en permanence. Cette fatigue n'est pas améliorée par le repos ou le sommeil.
-
-; symptomes secondaires :
-
-    ; une dévalorisation de soi, une perte de confiance en soi et d'estime de soi, un sentiment d'inutilité ;
-    ; un sentiment d'inutilité et une culpabilité excessive et injustifiée ;
-    ; une vision du futur et de la vie très négative avec des perspectives pessimistes ;
-    ; des pensées autour de la mort en général, et parfois autour du suicide ;
-    ; une diminution de la capacité de concentration, d'attention et de mémorisation ;
-    ; une dégradation du sommeil. Il devient peu réparateur, souvent trop court avec des réveils précoces, le matin ;
-    ; une perte de l'appétit qui peut entraîner un amaigrissement.
-
-; dépression chez les personnes âgées
-
-; Après l'accouchement, baby blues et dépression du post-partum
-
 (defparameter *questions* (list
-    '(humeur ("Comment décririez-vous votre humeur en ce moment ?") (joie tristesse colere neutre peur))
-    '(dureeJournee ("Cette humeur dure-t-elle toute la journée ou presque ?") (oui non))
-    '(constance ("La ressentez-vous pratiquement tous les jours depuis au moins deux semaines ?") (oui non))
-
-    '(troublePrincipal1 ("Avez-vous ressenti un abattement et une perte d'intérêt et de plaisir dans vos activités du quotidien et vos activités habituellement agréables ?") (oui non))
-
-    '(troublePrincipal2 ("Ressentez-vous une réduction de l'énergie ou une fatigabilité anormale ?" "Souffrez-voous d'une fatigue intense, souvent dès le matin ?" "Avez-vous l'impression de manquer d'énergie en permanence ?" "Cette fatigue n'est pas améliorée par le repos ou le sommeil.") (oui non))
-
-
-    '(troubleSecondaire1 ("Ressentez-vous une dévalorisation de soi, une perte de confiance en soi et d'estime de soi, un sentiment d'inutilité ?") (oui non))
-
-    '(troubleSecondaire2 ("Ressentez-vous un sentiment d'inutilité et une culpabilité excessive et injustifiée ?") (oui non))
-
-    '(vision ("Quelle est votre vision actuelle du futur et de la vie ?" "Est-elle très négative avec des perspectives pessimistes ?") (positive negative pessimiste optimiste neutre))
-
-    '(troubleSecondaire4 ("Avez-vous des pensées autour de la mort en général, et parfois autour du suicide ?") (oui non))
-
-    '(troubleSecondaire5 ("Avez-vous ressenti une diminution de la capacité de concentration, d'attention et de mémorisation ?") (oui non))
-
-    '(troubleSecondaire6 ("Avez-vous ressenti une dégradation du sommeil ?" "Il devient peu réparateur, souvent trop court avec des réveils précoces, le matin") (oui non))
-
-    '(troubleSecondaire7 ("Avez-vous ressenti une perte de l'appétit ?" "Celle-ci peut également entraîner un amaigrissement.") (oui non))
-
-
-
-    ; '(nombreTroublesPrincipaux ("nombre de troubles principaux") "nombre" numberp)
-    ; '(nombreTroublesSecondaires ("nombre de troubles secondaires") "nombre" numberp)
-    ; '(moisDepuisAccouchement "nombre de mois depuis l'accouchement" numberp)
-    ; '(symptomesPostnatale "nombre de symptomes de la dépression post-natale" numberp)
-    ; '(genre "votre genre ? (H/F)" (H F))
-    ; '(age "votre age ?" numberp)
-    ; '(troublePhysiqueOuCognitif "avez-vous un trouble physique ou cognitif ? (oui/non)" (oui non))
+    '("Quel type d'activité chechez-vous ? (intellectuelle ou physique)" 'type_activite)
+    '("Quel type d'activité intellectuelle chechez-vous ? (creative, culturelle, reflexive ou detente)" 'type_intellectuelle)
+    '("Quel art aimez-vous ? (visuel, musical ou litteraire)" 'art)
+    '("Quel type de reflexion aimez-vous ? (jeu de PLATEAU, jeu de CARTES, INFORMATIQUE)" 'reflexion)
+    '("Vous cherchez une activité pour combien de personnes ?" 'personnes)
+    '("Quel aspect vous attire le plus en informatique ? (software, hardware)" 'type_info)
+    '("Préférez-vous le NUMERIQUE ou le MANUEL ?" 'type_art_visuel)
+    '("Indiques le nombre d'heures que vous pouvez consacrer à l'activité (par session)" 'heure)
+    '("Préférez vous une activité culturelle chez vous (DOMICILE) ou à l'extérieur (EXTERIEUR) ?" 'lieu_culture)
+    '("Quel est votre budget par session en euros ?" 'budget)
+    '("Avec qui voulez-vous faire cette activité ? (FAMILLE, AMIS)" 'proches_activite)
+    '("Etes vous plutot jeux de SOCIETE ou jeux VIDEO ?" 'type_detente)
+    '("Quel type d'activité physique chechez-vous ? (sur TERRAIN, en INTERIEUR (et cours), SPECIALE (aquatique et hivernale) ou en EXTERIEUR)" 'type_physique)
+    '("Préférez-vous jouer au PIED ou a la MAIN ?" 'membre_physique)
+    '("Cherchez-vous une activité dans une COURS ou en INTERIEUR ?" 'type_pIntCours)
+    '("Quelle particularité préférez-vous parmis FILET, BUT, PANIER ?" 'type_pInterieure)
+    '("Cherchez-vous une activité dans AQUATIQUE ou HIVERNALE ?" 'type_pSpeciale)
+    '("Etes-vous proche d'une station de ski ? (OUI ou NON)" 'proche_station)
 ))
