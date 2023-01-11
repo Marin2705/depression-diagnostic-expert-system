73ecb5e1fe5bc569159367ed98fd220dd6eba869
CoulonLo <120654340+CoulonLo@users.noreply.github.com>
2023-01-11 20:50:15 +0100

    Update depression.lisp

--- depression.lisp
+++ depression.lisp
@@ -1,4 +1,6 @@
+(defparameter *BR* (list
 (add-activity '((intellectuelle)(creative)(visuel)(numerique)) "Photographie" "Evadez vous, capturez et observez mieux l'environnement qui vous entoure !")
   (add-activity '((intellectuelle)(creative)(visuel)(manuel)) "Peinture" "Evadez vous, capturez et observez mieux l'environnement qui vous entoure !")
   (add-activity '((intellectuelle)(creative)(musical)) "Composition musicale" "Faites parler votre créativité dans le domaine musicale !")
   (add-activity '((intellectuelle)(creative)(litteraire)) "Ecriture" "Prennez un papier et un crayon et exprimez-vous librement !")
+))
