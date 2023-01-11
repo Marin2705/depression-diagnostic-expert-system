cd0c33145d6994adc9f03cc1c6f83ed888711df2
CoulonLo <120654340+CoulonLo@users.noreply.github.com>
2023-01-11 19:31:57 +0100

    Update BR.lisp

--- BR.lisp
+++ BR.lisp
@@ -1,31 +1,56 @@
 ; Base de règles
 
 (defparameter *BR* (list
-    '(R1 ((>= nombreTroublesPrincipaux 2)) (= ressentTroublesPrincipaux T))
-    '(R2 ((>= nombreTroublesSecondaires 2)) (= ressentTroublesSecondaires T))
-    ; '(R ((= accouchementCetteAnnee T)) (= accouchementRecent T)) ; demander si déjà accouché avant de demander depuis combien de mois...
-    '(R3 ((<= moisDepuisAccouchement 6)) (= accouchementRecent T))
-    '(R4 ((>= symptomesPostnatale 1)) (= ressentSymptomesPostnatale T))
-    '(R5 ((= genre 'homme) (>= age 79)) (= personneAgee T))
-    '(R6 ((= genre 'femme) (>= age 85)) (= personneAgee T))
+    '(R1 ((type_activite eq intellectuelle)) 'triste))
+    '(R2 ((type_activite eq physique)) 'manque_interer))
+    '(R3 ((type_intellectuelle eq creative)) 'trouble_sommeil))
+    '(R4 ((type_intellectuelle eq culturelle)) 'trouble_appetit))
+    '(R5 ((type_intellectuelle eq reflexive)) 'difficulte_concentration))
+    '(R6 ((type_intellectuelle eq detente)) 'faible_estime))
+    '(R7 ((type_intellectuelle eq detente)) 'causes_externes_fortes))
 
-    '(R7 ((= ressentTroublesPrincipaux T) (= ressentTroublesSecondaires T)) (= etatDepressif T))
-    '(R8 ((= accouchementRecent T) (= ressentSymptomesPostnatale T)) (= etatDepressif T))
-    '(R9 ((= personneAgee T) (= troublePhysiqueOuCognitif T)) (= etatDepressif T))
-    
-    ; troubles principaux
-    '(R10 ((= troublePrincipal1 T) (= troublePrincipal2 T) (= troublePrincipal3 T)) (= ressentTroublesPrincipaux T))
-    '(R11 ((= troublePrincipal1 T) (= troublePrincipal2 T)) (= ressentTroublesPrincipaux T))
-    '(R12 ((= troublePrincipal1 T) (= troublePrincipal3 T)) (= ressentTroublesPrincipaux T))
-    '(R13 ((= troublePrincipal2 T) (= troublePrincipal3 T)) (= ressentTroublesPrincipaux T))
-    
-    ; trouble principal 1
-    '(R14 ((= humeur tristesse) (= dureeJournee oui) (= constance oui)) (= troublePrincipal1 T))
-    
-    ; trouble secondaire 3
-    '(R15 ((= vision pessimiste)) (= troubleSecondaire3 T))
-    '(R16 ((= vision negative)) (= troubleSecondaire3 T))
+;;La dépression réactionnelle
 
-    ; troubles secondaires
-    ;: TODO règle pour les 7 troubles secondaires (trop de combinaison, trouver comment les écrire)
-    ))
+    '(R8 ((type_intellectuelle eq detente)) 'perte_emotionnelle))
+    '(R9 ((type_intellectuelle eq detente)) 'ralentissement_psychomoteur))
+    '(R10 ((type_intellectuelle eq detente)) 'perte_libido))
+    '(R11 ((type_intellectuelle eq detente)) 'pas_causes_externes))
+    '(R12 ((type_intellectuelle eq detente)) 'intensité_symptomes_forte))
+
+;;La dépression endogène
+
+    '(R13 ((type_intellectuelle eq detente)) 'intensité_symptomes_faible))
+    '(R14 ((type_intellectuelle eq detente)) 'sensible_environnement)) 
+    '(R15 ((type_intellectuelle eq detente)) 'causes_externes_faibles))
+
+;;La dépression névrotique
+
+    '(R16 ((type_intellectuelle eq detente)) 'acouchement_4_8_semaines))
+
+
+;;La dépression du post-partum
+    
+    '(R17 ((type_intellectuelle eq detente)) 'vieu))
+    '(R18 ((type_intellectuelle eq detente)) 'vieille))
+    '(R19 ((type_intellectuelle eq detente)) 'mort_conjoint))
+    '(R20 ((type_intellectuelle eq detente)) 'maladie))
+    '(R21 ((type_intellectuelle eq detente)) 'faible_famille))
+
+;;La dépression de la personne âgée
+
+    '(R22 ((type_intellectuelle eq detente)) 'pas_depressif_en_general))
+    '(R23 ((type_intellectuelle eq detente)) 'saison_hiver))
+
+;;La dépression saisonnière
+
+    '(R24 ((type_intellectuelle eq detente)) 'continue))
+    '(R25 ((type_intellectuelle eq detente)) 'discontinue_bipolaire))
+
+;;La maniaco-dépression
+
+    '(R26 ((type_intellectuelle eq detente)) 'plaintes_régulières))
+    '(R27 ((type_intellectuelle eq detente)) 'symptomes_somatiques))
+    '(R28 ((type_intellectuelle eq detente)) 'symptomes_psycho_comportementaux ))
+    '(R29  ((type_intellectuelle eq detente)) 'non_symptomes_depressif ))
+
+;;La dépression masquée
